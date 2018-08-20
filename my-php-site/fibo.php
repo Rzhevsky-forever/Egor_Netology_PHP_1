2c5c9cab94b26f9597b9de1d0addfd0dd15066d9
Rzhevsky-forever <42357971+Rzhevsky-forever@users.noreply.github.com>
2018-08-21 00:48:18 +0500

    Add files via upload

---
+++ my-php-site/fibo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ввод числа для проверки</title>
+</head>
+<body>
+	<form action="proverka-fibo.php" method="get">
+		<p>Inter you namber fore checket</p>
+		<input type="text" name="x" value="">
+		<input type="submit" name="bsubmit" value="Отправить" />
+	</form>
+</body>
+</html>
