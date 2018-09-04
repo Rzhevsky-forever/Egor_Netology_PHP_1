328caf460fe1a6fefbec5e111b6ccab8d1bb2415
Rzhevsky-forever <42357971+Rzhevsky-forever@users.noreply.github.com>
2018-09-05 01:20:07 +0500

    Add files via upload

--- netology_php_hw_lesson_5/list.php
+++ netology_php_hw_lesson_5/list.php
@@ -10,25 +10,21 @@
 <?php
 //Получаем данные из test.json
 $test_list_j = file_get_contents(__DIR__ . "./test.json", true);
-echo $test_list_j;
+//echo $test_list_j; // Промежуточный вывод
 $test_list = json_decode($test_list_j, TRUE);
-var_dump($test_list);
-$a = 1;
-$b = 0;
-    foreach ($test_list as $name => $value) :
-        switch ($a) :
-            case $a == 1: // Когда $a задавалась 0 и проверка шла на равеноство (==) или (===) нулю этот код не работал
-            $a = $name;
-            case $a != 1:
-            $b = $name;
-        endswitch;
-    endforeach;
-    ?>
-    <form action="test.php" method="get">
-        <input type="radio" name="a" value="<?php echo $a; ?>"><?php echo $a; ?>
-        <input type="radio" name="b" value="<?php echo $b; ?>"><?php echo $b; ?>
-        <input type="submit">
-    </form>
-        
-</body>
+//var_dump($test_list); // Промежуточный вывод
+
+$i = 0; // Это я так передаю заголовки тестов в переменные для вывода
+foreach ($test_list as $key => $v) { 
+    if ($i < 1) { $a = $key; }  
+    $i ++; // Такое решение лучше предидущего
+    if ($i > 1) { $b = $key; }
+}
+?>
+        <form action="test.php" method="get">
+            <input type="checkbox" name="a" value="<?php echo $a; ?>"><?php echo $a; ?>
+            <input type="checkbox" name="b" value="<?php echo $b; ?>"><?php echo $b; ?>
+            <input type="submit">
+        </form>
+    </body>
 </html>
