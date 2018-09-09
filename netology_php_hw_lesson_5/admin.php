041cf7ab4f614c9f1c8eed1436a7268a4197067e
Rzhevsky-forever <42357971+Rzhevsky-forever@users.noreply.github.com>
2018-09-09 17:27:01 +0500

    Add files via upload

--- netology_php_hw_lesson_5/admin.php
+++ netology_php_hw_lesson_5/admin.php
@@ -6,21 +6,63 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <meta charset="UTF-8" lang="RU">
+        <title>Admin</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
         <?php
-            $form = [
-                'test-1'=>['a1' => 8, 'a2' => 16, 'a3' => 60, 'a4' => 100],
-                'test-2'=>['b1' => 10, 'b2' => 20, 'b3' => 30, 'b4' => 40]
-            ];
-            //var_dump($form); // Промежуточный контрольный вывод массива
-            $json_form = json_encode($form);
-            // echo $json_form; // Промежуточный контрольный вывод строчной формы для JSON
-            file_put_contents(__DIR__ . '/test.json', $json_form);//Создаем и одновременно пишем файл test.json
-            //echo file_get_contents('test.json'); // Тестовый вывод
+        $fileName = "uploadfile";
+        ?>
+        <form method="POST" enctype="multipart/form-data" action="admin.php">
+            <p>Выбирите файл .json</p>
+            <div><input type="file" name="<?php echo $fileName ?>" multiple></div>
+            <div><input type="submit"</div>
+        </form>
+        <?php
+        if (!empty($_FILES)) {
+            var_dump($_FILES); // Test print
+            if ($_FILES[$fileName]['type'] === 'application/json'){
+                echo ($_FILES[$fileName]['type']) . "<br>"; // Test print
+                $testName = $_FILES[$fileName]["name"];
+                echo "<br>"."<br>"."<br>"."<br>"."<br>"."<br>".$testName;
+                move_uploaded_file($_FILES[$fileName]["tmp_name"], $_FILES[$fileName]["name"]); // Перемещаем файл из директория по умолчанию в директорию, где admin.php
+        }   else {
+                echo 'Проблемма... Загрузите файл в запрошенном формате ';
+            } 
+        }   
+        else {
+            echo 'Файл не был загружен. Загрузите файл и повторите отправку';
+        }
+        
+        
+//-----------СТАРОЕ **НЕ** ПРАВИЛЬНОЕ РЕШЕНИЕ---------------
+// . $_FILES[$fileName]["name"]        
+//        $u = 'qw'; // Работ с массивом
+//        $t = 'er';
+//        $o = 'po';
+//        $z = 'rt';
+//        $a = ['name' => [$u => $t, $z => $o]];
+//        if ($a['name'][$u] == $t) {
+//            echo $a['name'][$z];
+//        };
+//        $uploadDir = './testsDir/';
+//        $uploadfile = $uploadDir.basename($_FILES['uploadfile']['name']);
+//        
+//        if (is_uploaded_file($_FILES ['uploadfile']['tmp_name'])){
+//            copy($_FILES ['test-fileName']['tmp_name'], $uploadDir);
+//        }
+//            $form = [
+//                'test-1'=>['a1' => 8, 'a2' => 16, 'a3' => 60, 'a4' => 100],
+//                'test-2'=>['b1' => 10, 'b2' => 20, 'b3' => 30, 'b4' => 40]
+//            ];
+//            //var_dump($form); // Промежуточный контрольный вывод массива
+//            $json_form = json_encode($form);
+//            // echo $json_form; // Промежуточный контрольный вывод строчной формы для JSON
+//            file_put_contents(__DIR__ . '/test.json', $json_form);//Создаем и одновременно пишем файл test.json
+//            //echo file_get_contents('test.json'); // Тестовый вывод
             
         ?>
+        
     </body>
 </html>
--- netology_php_hw_lesson_5/list.php
+++ netology_php_hw_lesson_5/list.php
@@ -2,29 +2,42 @@
 
 <html>
     <head>
-        <meta charset="UTF-8">
+        <meta charset="UTF-8" lang="RU">
         <title></title>
     </head>
     <body>
         
 <?php
-//Получаем данные из test.json
-$test_list_j = file_get_contents(__DIR__ . "./test.json", true);
-//echo $test_list_j; // Промежуточный вывод
-$test_list = json_decode($test_list_j, TRUE);
-//var_dump($test_list); // Промежуточный вывод
+    $testName = "\\D:\wamp64\www\netology_php_hw_lesson_5" . $testName;
+    echo $testName;
+//    var_dump($_FILES); // Test print
+//    var_dump($_POST); // Test print
+    if (file_exists($testName)) {
+        ?>  <div>
+            <form method="POST">
+                <input type="radio" name="<?php echo $testName; ?>" value="xz">
+            </form> 
+            </div>
+   <?php     
+    }
+    
+    
+    
 
-$i = 0; // Это я так передаю заголовки тестов в переменные для вывода
-foreach ($test_list as $key => $v) { 
-    if ($i < 1) { $a = $key; }  
-    $i ++; // Такое решение лучше предидущего
-    if ($i > 1) { $b = $key; }
-}
+//-----------СТАРОЕ **НЕ** ПРАВИЛЬНОЕ РЕШЕНИЕ---------------
+////Получаем данные из test.json
+//$test_list_j = file_get_contents(__DIR__ . "./test.json", true);
+////echo $test_list_j; // Промежуточный вывод
+//$test_list = json_decode($test_list_j, TRUE);
+////var_dump($test_list); // Промежуточный вывод
+//
+//$i = 0; // Это я так передаю заголовки тестов в переменные для вывода
+//foreach ($test_list as $key => $v) { 
+//    if ($i < 1) { $a = $key; }  
+//    if ($i > 1) { $b = $key; }
+//    $i = $i+2; // Такое решение лучше предидущего
+//}
 ?>
-        <form action="test.php" method="get">
-            <input type="checkbox" name="a" value="<?php echo $a; ?>"><?php echo $a; ?>
-            <input type="checkbox" name="b" value="<?php echo $b; ?>"><?php echo $b; ?>
-            <input type="submit">
-        </form>
+        
     </body>
 </html>
